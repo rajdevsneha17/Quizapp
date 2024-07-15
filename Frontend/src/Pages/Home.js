d8034f05479a593dbf1ec8d2b789c2a3c76d08e3
rajdevsneha17 <rajdevsneha10@gmail.com>
2024-07-15 12:51:39 +0530

    yourself

--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
       
       <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full text-center mx-4 sm:mx-6 lg:mx-8">
         <h1 className="text-2xl sm:text-3xl font-bold mb-4">Welcome to our Quiz Portal!</h1>
-        <p className="text-base sm:text-lg mb-6">Take the assessment to discover your knowledge about Web Development.</p>
+        <p className="text-base sm:text-lg mb-6">Take the assessment to discover the knowledge about Yourself.</p>
         <Link to="/assessment">
           <button className="bg-blue-500 text-white py-2 px-4 rounded shadow hover:bg-blue-600 hover:shadow-lg transition duration-300">
             Take Assessment
